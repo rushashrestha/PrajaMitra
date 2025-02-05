aedfca74d747154f0e75dde45427d1cfbdadab42
Rusha Shrestha <shrestharusha194@gmail.com>
2025-02-05 12:57:46 +0545

    merge success

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login/login";
 import Home from "./Components/Pages/Home";
-<<<<<<< HEAD
 import Footer from "./Components/Footer/Footer";
-=======
 import Signup from "./Components/signup/signup";
->>>>>>> 7e57db4b6f59ee2b4a4d8ad4e5cb40f0495c1c8a
 
 
 function App() {
