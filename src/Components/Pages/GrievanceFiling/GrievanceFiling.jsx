eb716e8c1cb090219b1f735266bce850ff04f714
rusma07 <shrestharusu5@gmail.com>
2025-02-05 18:50:03 +0545

    grievance form

--- src/Components/Pages/GrievanceFiling/GrievanceFiling.jsx
+++ src/Components/Pages/GrievanceFiling/GrievanceFiling.jsx
@@ -3,12 +3,28 @@ import './Grievance.css'
 const GrievanceFiling = () => {
   return (
     <div className="grievanceFiling">
-      <h3>Grievance Filing Form</h3>
+      <h3 style={{textAlign:'center'}}>Grievance Filing Form</h3>
       <form action="">
-        <label htmlFor="gName">Name(first name, middle name and last name):</label>
-        <input type="text" name="gName" id="gName" placeholder="Please enter your full name"/>
+      <div className="form">        
+        <label htmlFor="gName">Name(first name, middle name and last name):<input type="text" name="gName" id="gName" /></label>
+        
+        <div className="rga">
+        <label htmlFor="Nationality">Nationality: <input id="race" type="text" /></label>
+        <label htmlFor="Gender">Gender: <input type="text" id="gGender" /></label>
+        <label htmlFor="Age">Age: <input type="number" id="age"/></label>
+        </div>
+        
+        <label htmlFor="MailingAddress">Mailing Address(include): <input type="text" id="mailingAddress"/></label>
+        <label htmlFor="gEmail">Email address: <input type="text" /></label>
+
+        <label htmlFor="issues">Grievable Issue: <input type="text" id="issues" /></label>
+
+        <label htmlFor="breif">Brief statement of issues and facts on which grievance is based: </label><textarea name="brief" id="breif"></textarea> 
+        <label htmlFor="resolution">Desired Relief or Resolution:</label>
+        <textarea name="resolution" id="resolution"></textarea>
+        <button className="btnSubmit">Submit</button>
+      </div>
       </form>
-      {/* Add your bill payment functionality here */}
     </div>
   );
 };
